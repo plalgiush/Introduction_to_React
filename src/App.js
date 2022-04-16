7e266b80d4984b7a02692c9a268fd635c7f9818f
plalgiush <ilyasushko@yandex.ru>
2022-04-16 23:43:57 +0300

    a more complex state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-const Hello = ({ name, age }) => {
-    const bornYear = () => new Date().getFullYear() - age
+import { useState } from "react";
+import button from "./Components/Button";
+import {logDOM} from "@testing-library/react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+      return (
+          <div>
+              the app is used by pressing thr buttons
+          </div>
+      )
+  }
+  return (
+      <div>
+          button press history: {props.allClicks.join(' ')}
+      </div>
+  )
+}
+
+const Button = ( {handleClick, text} ) => {
     return (
-        <div>
-            <p>
-                Hello {name}, you are {age} years old
-            </p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
+        <button onClick={handleClick}>{text}</button>
     )
-};
+}
 
 const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const [value, setValue] = useState(10)
+
+    const setToValue = (newValue) => () => {
+        console.log(newValue)
+        setValue(newValue)
+    }
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 26} />
-            <Hello name={name} age={age} />
+            {left}
+            <Button handleClick={handleLeftClick} text='left' />
+            <Button handleClick={handleRightClick} text='right' />
+            {right}
+            <History allClicks={allClicks} />
+
+            {value}
+            <button onClick={setToValue(1000)}>thousand</button>
+            <button onClick={setToValue(0)}>zero</button>
+            <button onClick={setToValue(value + 1)}>increment</button>
         </div>
     )
 }
